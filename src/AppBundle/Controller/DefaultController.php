a681d3101a032ea09ff9ce6a220804d0aadcc1b4
Francois <maps_red@yahoo.fr>
2016-05-05 01:08:29 +0200

    Add views and Update content

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,42 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $twigArray['quotes'] = $this->getDoctrine()->getRepository("AppBundle:Quote")->findAll();
+        return $this->render("AppBundle:default:homepage.html.twig", $twigArray);
+    }
+
+    /**
+     * @Route("/{type}-{nb}", name="type_quote", requirements={
+     *  "nb": "\d+",
+     *  "type": "top|flop"
+     *  })
+     * @param Request $request
+     * @param $type
+     * @param $nb
+     * @return Response
+     */
+    public function orderAction(Request $request, $type, $nb)
+    {
+        $type = $type == "flop" ? "DESC" : "ASC";
+    }
+
+    /**
+     * @Route("/quote/new", name="new_quote")
+     * @return Response
+     */
+    public function createQuoteAction()
+    {
+
     }
 }
